6abfac3d3f66d873fec27be9a74091f9da06d979
welchs0102 <welchs900102@gmail.com>
2024-06-27 17:05:15 +0900

    file upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PhotoUpload from './PhotoUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Photo Upload Example</h1>
+      <PhotoUpload />
+    </div>
+  );
+}
+
+export default App;
